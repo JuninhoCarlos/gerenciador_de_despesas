07b9c178c028be9080fc22521b2765a48c02953e
Juninho carlos <juninho.ufpi@hotmail.com>
2020-06-24 19:41:05 -0300

    adicionando o codigo do backend

---
+++ api/despesas/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nome
+
+
+class Despesa(models.Model):
+    valor = models.FloatField()
+    data  = models.DateField() 
+    descricao = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descricao
